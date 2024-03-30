a3300a4ffc6c8139d64eca995d84ff116bd8381a
mjfactor <emjayfactor@gmail.com>
2024-03-30 14:43:21 +0800

    update

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
-with open('README.md', 'r') as file:
+with open('readme.md', 'r') as file:
     content = file.read()
 
 
 content = content.replace('Dynamic Programming', 'Docker').replace('Docker', 'Dynamic Programming', 1)
 
-with open('README.md', 'w') as file:
+with open('readme.md', 'w') as file:
     file.write(content)
