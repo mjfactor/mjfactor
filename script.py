4693fd1568a9bfa06085f5d0e4b3ffb8bb06534d
Emjay Factor <emjayfactor@gmail.com>
2024-12-12 13:07:18 +0800

    Update script.py

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ with open('readme.md', 'r') as file:
     content = file.read()
 
 
-content = content.replace('Dynamic Programming', '###REPLACE###').replace('Docker', 'Dynamic Programming').replace('###REPLACE###', 'Docker', 1)
+content = content.replace('Dynamic Programming', '###REPLACE###').replace('Docker', 'Dynamic Programming').replace('###REPLACE###', 'Docker')
 
 
 with open('readme.md', 'w') as file:
